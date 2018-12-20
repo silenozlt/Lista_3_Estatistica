1d2e3f631de412d594174c48a355801094032889
Cassio Philipe Placido <silenozlt@hotmail.com>
2018-12-20 20:23:32 -0200

    Create script-lista3.R

---
+++ script-lista3.R
@@ -0,0 +1,10 @@
+######################################
+## LISTA 3
+
+rm(list=ls(all=TRUE))
+
+setwd("/Users/cassio/Dropbox/Pos Data Science/Lista_3_Estatistica")
+
+# Carregando os dados:
+trabalho <- read.table("trabalho.txt")
+View(trabalho)
